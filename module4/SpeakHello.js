1d2d07dc91c7d2a9bb2367db482caa334b630edd
daksh <35658838+daksh6131@users.noreply.github.com>
2020-08-10 22:35:15 +0530

    Update SpeakHello.js

--- module4/SpeakHello.js
+++ module4/SpeakHello.js
@@ -1,16 +1,12 @@
 
 (function(window){
-var helloSpeaker={};
+var helloSpeaker = {};
 var speakWord = "Hello";
-helloSpeaker.speak = function speak(name) {
+helloSpeaker.speak = function(name) {
   console.log(speakWord + " " + name);
 }
-window.helloSpeaker=helloSpeaker;
-
-
-
+window.helloSpeaker=helloSpeaker ;
 })(window);
-
 
 
 
