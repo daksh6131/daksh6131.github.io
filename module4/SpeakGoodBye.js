893e4196e22bf294dd831e077f329536e1032d9e
daksh <35658838+daksh6131@users.noreply.github.com>
2020-08-10 22:40:42 +0530

    Update SpeakGoodBye.js

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -1,9 +1,9 @@
 (function(window){
 	var byeSpeaker={};
 	var speakWord = "Good Bye";
-	byeSpeaker.speak = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-	window.byeSpeaker=byeSpeaker;
+byeSpeaker.speak= function (x) {
+  console.log(speakWord + " " +x);
+}
+window.byeSpeaker = byeSpeaker;
 })(window);
   
